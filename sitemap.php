375e396de457fe0ea3fb8c9a3c86349a662a6c29
qianduan <mienflying@gmail.com>
2015-01-16 13:41:12 +0800

    add sitemap

--- sitemap.php
+++ sitemap.php
@@ -25,18 +25,32 @@ get_header(); ?>
 		<?php endif; ?>
 			<header class="article-header">
 				<h1 class="article-title"><?php the_title(); ?></h1>
-			</header><!-- .article-header -->
-
+			</header>
 			<div class="article-content">
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'eastBlue' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-			</div><!-- .article-content -->
-
-			<footer class="article-meta">
-				<?php edit_post_link( __( 'Edit', 'eastBlue' ), '<span class="icon-edit">', '</span>' ); ?>
-			</footer>
-		</article><!-- #post -->
-<?php comments_template(); ?>
+				<h3>Pages</h3>
+				<ul>
+					<?php wp_list_pages('depth=1&sort_column=menu_order&title_li=' ); ?>
+				</ul>
+				<h3>Categories</h3>
+				<ul>
+					<?php wp_list_categories('title_li=&hierarchical=0&show_count=1') ?>
+				</ul>
+			<?php 
+				$cats = get_categories(); 
+				foreach ($cats as $cat) {
+				query_posts('cat='.$cat->cat_ID);
+				?>
+				<h3><?php echo $cat->cat_name; ?></h3>
+				<ul>
+					<?php while (have_posts()) : the_post(); ?>
+					<li style="font-weight:normal !important;"><a href="<?php the_permalink() ?>">
+						<?php the_title(); ?>
+						</a> - Comments (<?php echo $post->comment_count ?>)</li>
+					<?php endwhile;  ?>
+				</ul>
+			<?php } ?>
+			</div>
+		</article>
 		<?php endwhile; ?>
 	</div><?php get_sidebar(); ?>
 </div><!-- #primary -->
